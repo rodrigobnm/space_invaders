88341d54d819de5aea071776fa1da2a916f9d369
rodrigobnm <150079017+rodrigobnm@users.noreply.github.com>
2024-05-17 14:15:12 +0000

    .

--- .ccls-cache/@home@runner@spaceinvaders/projeto@src@main.c
+++ .ccls-cache/@home@runner@spaceinvaders/projeto@src@main.c
@@ -1,95 +1,147 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
-#include <string.h>
+#define WIDTH 100
+#define HEIGHT 50
 
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
+int playerX, bulletX, bulletY, enemyX, enemyY, gameOver;
+char screen[HEIGHT][WIDTH];
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
+int kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
 
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(RED, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("W");
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
 }
 
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
-
-    screenGotoxy(34, 23);
-    printf("");
-    
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
+void initialize() {
+  int i, j;
+  playerX = WIDTH / 2;
+  bulletX = -1;
+  bulletY = -1;
+  enemyX = rand() % (WIDTH - 2) + 1;
+  enemyY = 1;
+  gameOver = 0;
+  for (i = 0; i < HEIGHT; i++) {
+    for (j = 0; j < WIDTH; j++) {
+      if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1)
+        screen[i][j] = '#';
+      else
+        screen[i][j] = ' ';
     }
+  }
+  screen[HEIGHT - 2][playerX] = '|';
+}
+
+void draw() {
+  system("clear || printf '\\033c'");
+  int i, j;
+  for (i = 0; i < HEIGHT; i++) {
+    for (j = 0; j < WIDTH; j++) {
+      printf("%c", screen[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void update() {
+  if (kbhit()) {
+    char key = getchar();
+    if (key == 'a' && playerX > 1) {
+      screen[HEIGHT - 2][playerX] = ' ';
+      playerX--;
+      screen[HEIGHT - 2][playerX] = '|';
+    } else if (key == 'd' && playerX < WIDTH - 2) {
+      screen[HEIGHT - 2][playerX] = ' ';
+      playerX++;
+      screen[HEIGHT - 2][playerX] = '|';
+    } else if (key == ' ' && bulletY == -1) {
+      bulletX = playerX;
+      bulletY = HEIGHT - 3;
+    }
+  }
+  if (bulletY >= 1) {
+    screen[bulletY][bulletX] = ' ';
+    bulletY--;
+    if (bulletY == enemyY && bulletX == enemyX) {
+      bulletY = -1;
+      enemyX = rand() % (WIDTH - 2) + 1;
+      enemyY = 1;
+    } else {
+      screen[bulletY][bulletX] = '*';
+    }
+  } else if (bulletY == 0) {
+    bulletY = -1;
+    screen[bulletY][bulletX] = ' ';
+  }
+
+  // Movimento do inimigo
+  if (enemyY < HEIGHT - 2) {
+    screen[enemyY][enemyX] = ' ';
+    enemyY++;
+    screen[enemyY][enemyX] = 'Z';
+
+    // Checa colisão com o jogador
+    if (enemyY == HEIGHT - 2 && enemyX == playerX) {
+      gameOver = 1;
+    }
+  } else {
+    // Se o inimigo atingir o chão, reinicia sua posição
+    enemyX = rand() % (WIDTH - 2) + 1;
+    enemyY = 1;
+  }
+}
+
+void gameOverScreen() {
+  system("clear || printf '\\033c'");
+  printf("Game Over!\n");
+}
+
+void menu() {
+  printf("Pressione a tecla enter para jogar.\n");
+  getchar();
 }
 
 int main() {
-    
-    // Inicialize variáveis
-    size_t maxX = MAXX;
-    size_t minX = MINX;
-    static int ch = 0;
-
-    // Inicialize o ambiente (tela, teclado, temporizador)
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-
-    printHello(x, y);
-    screenUpdate();
-
-    while (ch != 10) //enter
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (maxX - strlen("W") - 1) || newX <= minX + 1) 
-                incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) 
-                incY = -incY;
-
-            printKey(ch);
-            printHello(newX, newY);
-
-            screenUpdate();
-        }
+  int playAgain = 1;
+  while (playAgain) {
+    menu();
+    initialize();
+    while (!gameOver) {
+      draw();
+      update();
+      usleep(100000);
     }
-
-    // Limpeza do ambiente (liberação de recursos)
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
-
-    return 0;
+    gameOverScreen();
+    printf("Selecione Enter para jogar novamente ou 's' para sair.\n");
+    char input = getchar();
+    if (input == 's' || input == 'S')
+      playAgain = 0;
+    else {
+      gameOver = 0;
+      getchar(); // Consumir o caractere de nova linha
+    }
+  }
+  return 0;
 }
